eae875402c05302c6b4e50507e78c28bc7fa1351
Jonas Bjørge Andersen <dev@jonasandersen.no>
2025-07-03 22:08:00 +0200

    Added script for git setup and list of programs

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # stuff
+
+
+# Programs
+- 1password
+- chezmoi
+- gh
+- git
+- fish 
+- direnv
+- starship
+- maven
+- java
+- go
---
+++ setup-git.fish
@@ -0,0 +1,4 @@
+#!/usr/bin/env fish
+
+git config --global user.email "dev@jonasandersen.no"
+git config --global user.name "Jonas Bjørge Andersen"
